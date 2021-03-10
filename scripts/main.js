720d5a27a5dce4baebaaa56ecf8c7ff58d1318f2
juan880630 <juan.hastier@gmail.com>
2021-03-10 07:58:37 -0500

    Correcciones a los directorios

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,10 @@ const miImagen = document.querySelector("img");
 
 miImagen.onclick = function () {
     let srcImage = miImagen.getAttribute("src");
-    if (srcImage === "/images/image-1.jpg") {
-        miImagen.setAttribute("src", "/images/image-2.jpg");
+    if (srcImage === "images/image-1.jpg") {
+        miImagen.setAttribute("src", "images/image-2.jpg");
     } else {
-        miImagen.setAttribute("src", "/images/image-1.jpg")
+        miImagen.setAttribute("src", "images/image-1.jpg")
     }
 }
 
